f2e85f229ab77c128ef9e819e8cfc7b765558148
Lee Wisener <wisenerl@Lees-Mac-Studio.local>
2025-07-22 20:32:25 +0100

    My changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,8 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-    
-
+        $schedule->command('backup:clean')->daily()->at('01:00');
     }
 
     /**
